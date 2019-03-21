8b34676ac5a196fd9dddff7369acbaba3bf84bd4
Ewan Slater <ewan.slater@gmail.com>
2019-03-21 11:22:05 +0000

    simplified invoke_function

--- invoke_function_file.rb
+++ invoke_function_file.rb
@@ -1,16 +1,18 @@
-require_relative "./api_helper"
+require_relative './api_helper'
 
-r = "usage: ruby invoke_function.rb <compartment-name> <app-name> " \
-    "<function-name> [<request-payload>]"
+r = 'usage: ruby invoke_function.rb <compartment-name> <app-name> ' \
+    '<function-name> [<request-payload>]'
 begin
-  case ARGV.length
-  when 4
-    r = invoke_function(compartment_name: ARGV[0], app_name: ARGV[1], function_name: ARGV[2], payload: ARGV[3]).data
-  when 3
-    puts "Invocation with no payload"
-    r = invoke_function(compartment_name: ARGV[0], app_name: ARGV[1], function_name: ARGV[2]).data
+  if ARGV.length.between?(3, 4)
+    r = invoke_function(
+      compartment_name: ARGV[0],
+      app_name: ARGV[1],
+      function_name: ARGV[2],
+      payload: ARGV.fetch(3, '')
+    ).data
   end
   puts r
-rescue => e
+rescue StandardError => e
+  puts e.backtrace if ENV['DEBUG']
   puts "An error occurred: #{e.message}"
 end
